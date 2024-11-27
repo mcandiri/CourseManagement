2489d407d4489d109c206ac4c69faa87eaca1954
PC <PC@DESKTOP-QG8O7R7>
2024-11-27 14:07:36 +0300

    LongestIncreasingSubarray algorithm problem solved.

--- CourseManagement/AlgorithmTasks/LongestIncreasingSubarray.cs
+++ CourseManagement/AlgorithmTasks/LongestIncreasingSubarray.cs
@@ -26,13 +26,51 @@
         /// </note>
         public static List<int> FindLongestIncreasingSubarray(List<int> numbers)
         {
-            // TODO: Implement the logic to find the longest increasing subarray.
-            // - Use a loop to iterate through the list while keeping track of the current increasing subarray.
-            // - When the sequence breaks, compare the current subarray's length to the longest found so far.
-            // - If there are multiple subarrays with the same length, return any one of them.
-            // - At the end of the loop, return the longest subarray found.
-            return [];
-        }
 
+            //Checks if the list is null or empty.
+            if (numbers == null || numbers.Count == 0)
+            {
+                return new List<int>();
+            }
+
+            //A substring and a long substring are created.
+            List<int> subarray = new();
+            List<int> longSubarray = new();
+
+            //The variable to which we give the number at the index in the foreach loop.
+            int previousNumber = 0;
+
+            //We created a loop inside the list.
+            foreach (var number in numbers)
+            {
+                //We check if the incoming number is greater than the previous number.
+                if (number > previousNumber)
+                {
+                    subarray.Add(number);
+                }
+                else
+                {
+                    //If the increase stops, we check if the current list is larger than the long list.
+                    if (subarray.Count > longSubarray.Count)
+                    {
+                        longSubarray = new List<int>(subarray);
+                    }
+                    //If subarray is not greater than longSubarray, we delete the subarray list.
+                    //We add the new number
+                    subarray.Clear();
+                    subarray.Add(number);
+                }
+                //The number is added as the old number.
+                previousNumber = number;
+
+            }
+
+            //After the loop we check if the remaining subarray is greater than longSubarray.
+            if (subarray.Count > longSubarray.Count)
+            {
+                longSubarray = new List<int>(subarray);
+            }
+            return longSubarray;
+        }
     }
 }
