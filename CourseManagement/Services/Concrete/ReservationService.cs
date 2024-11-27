8cb7b6d03b9da9599642365c00038900c1cb2a15
PC <PC@DESKTOP-QG8O7R7>
2024-11-27 04:01:54 +0300

    The logic of a student registering for a course was implemented.

--- CourseManagement/Services/Concrete/ReservationService.cs
+++ CourseManagement/Services/Concrete/ReservationService.cs
@@ -1,4 +1,5 @@
-﻿using CourseManagement.Repositories.Abstract;
+﻿using CourseManagement.Models;
+using CourseManagement.Repositories.Abstract;
 using CourseManagement.Services.Abstract;
 
 namespace CourseManagement.Services.Concrete
@@ -11,11 +12,37 @@ namespace CourseManagement.Services.Concrete
 
         public async Task<bool> RegisterStudentToCourseAsync(int courseId, int studentId)
         {
-            // TODO: Implement the logic to register a student to a course.
-            // 1. Check if the course capacity is available.
-            // 2. Check if the student has already registered for this course.
-            // 3. Update the available slots of the course.
-            // 4. Add the StudentCourse record.
+            //Information about the course you want to register for is obtained.
+            var course = await _courseRepository.GetByIdAsync(courseId);
+
+            //Student information is obtained.
+            var student = await _studentRepository.GetByIdAsync(studentId);
+
+            //A DB query is made to check whether the student has previously registered for this course.
+            var studentCourse = await _studentCourseRepository.FindAsync(x => x.StudentId == studentId && x.CourseId == courseId);
+
+            //Null check is done according to course and student information and the course quota is checked and  if the student is not registered, we expect null.
+            if (course == null || student == null || course.AvailableSlots == 0 || studentCourse != null)
+            {
+                return false;
+            }
+
+            //Course quota is reduced by 1 person.
+            course.AvailableSlots -= 1;
+
+            //Course information is updated.
+            await _courseRepository.UpdateAsync(course);
+
+            StudentCourse newStudentCourse = new()
+            {
+                CourseId = courseId,
+                StudentId = studentId,
+                EnrollmentDate = DateTime.Now
+            };
+
+            //The student is registered for the desired course.
+            await _studentCourseRepository.AddAsync(newStudentCourse);
+
             return true; // Returning true as a placeholder
         }
 
